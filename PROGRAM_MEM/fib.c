030fdf6d97e63d2635696a7ca8dc09cb1b884ccf
Caio E. F. de Miranda <116667039+Aushiffer@users.noreply.github.com>
2023-11-16 11:05:15 -0300

    Update fib.c

--- PROGRAM_MEM/fib.c
+++ PROGRAM_MEM/fib.c
@@ -7,7 +7,6 @@ int main() {
 	int n;
 	printf("Escreva quantos números da seq. de Fibonacci você quer mostrar (após 0 e 1): ");
 	scanf("%d", &n);
-	printf("%d %d ", fib1, fib2);
 	for (int i = 0; i < n; i++) {
 		fib3 = fib1 + fib2;
 		printf("%d ", fib3);
